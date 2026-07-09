d78a9d9644cf97f4fe32b6fa2aa83397f997fef0
Rajesh <rakhiyadavmalyala@gmail.com>
2026-07-09 17:30:43 +0530

    Refactor LanguageFilterItem component syntax

--- src/components/lanuageFilterItem/index.js
+++ src/components/lanuageFilterItem/index.js
@@ -1,25 +1,25 @@
 import './index.css'
 
 const LanguageFilterItem = props => {
-    const { filterDetails, isActive, changeLanguage } = props
-    const { id, language } = filterDetails
-    const activeClass = isActive ? 'active-btn' : ''
+  const {filterDetails, isActive, changeLanguage} = props
+  const {id, language} = filterDetails
+  const activeClass = isActive ? 'active-btn' : ''
 
-    const onClickLanguage = () => {
-        changeLanguage(id)
-    }
+  const onClickLanguage = () => {
+    changeLanguage(id)
+  }
 
-    return (
-        <li>
-            <button
-                type="button"
-                className={`language-btn ${activeClass}`}
-                onClick={onClickLanguage}
-            >
-                {language}
-            </button>
-        </li>
-    )
+  return (
+    <li>
+      <button
+        type="button"
+        className={`language-btn ${activeClass}`}
+        onClick={onClickLanguage}
+      >
+        {language}
+      </button>
+    </li>
+  )
 }
 
 export default LanguageFilterItem
